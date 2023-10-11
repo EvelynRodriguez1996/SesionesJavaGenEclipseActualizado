421436af6f084f7e350c56d6dd094ecc8880971f
Evelyn Casillas <sistem.out.println.hello.evelyn@gmail.com>
2023-10-10 23:11:49 -0600

    Se añade actividad de la esfera

---
+++ Figuras/src/Figura/Calculable.java
@@ -0,0 +1,16 @@
+package Figura;
+
+public interface Calculable {
+
+	//Area, Volumen, Perimetro
+	
+
+	//Metodos de la interface
+	double calcularArea();
+	
+	double calcularVolumen();
+	
+	double calcularPerimetro();
+	
+	
+}//interface Calculable
